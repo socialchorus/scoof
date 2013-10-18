e4fda866e760dac769f8c55209a48993734de14e
Ian Cooper & Kane Baccigalupi <pair+ian+kane@socialchorus.com>
2013-10-18 06:50:18 -0700

    adds text utils: camelize, underscore, shorten and stripTags

--- app/assets/javascripts/scoof.js
+++ app/assets/javascripts/scoof.js
@@ -3,6 +3,7 @@
 //= require hogan
 //
 //= require scoof/scoof
+//= require_tree ./scoof/utils
 //= require scoof/overrides/function_bind
 //= require scoof/overrides/auth_token_adder
 //= require scoof/overrides/global_publisher_adder
